3e4e2095caeb4abce1b930bb6b0e5af8e3b07eab
cindy4127 <103284173+cindy4127@users.noreply.github.com>
2022-04-22 12:56:05 -0700

    commands for Junit
    javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
    
    java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest

---
+++ MarkdownParseTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;       // imports Junit assert method
+import org.junit.*;                    // imports Junit
+public class MarkdownParseTest {      // creates class for testing
+    @Test                            // informs Junit that we will be testing
+    public void addition() {        // Test case that will check for a specfic case (addition)
+        assertEquals(2, 1 + 1);    // checks to see if the output matches the expected output
+
+    }
+}
+
